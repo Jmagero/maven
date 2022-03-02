9cf48378279f7c8ff4a6334e4d80aa26575b7a92
Jocyline Magero <jmagero54@gmail.com>
2022-03-02 11:53:02 -0600

    Annotation Based Configuration

--- Marven/src/main/java/com/jmagero/Marven/Bike.java
+++ Marven/src/main/java/com/jmagero/Marven/Bike.java
@@ -1,5 +1,8 @@
 package com.jmagero.Marven;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Bike implements Vehicle {
 	public void drive() {
 		System.out.print("I am riding a bike");
--- Marven/src/main/java/com/jmagero/Marven/Car.java
+++ Marven/src/main/java/com/jmagero/Marven/Car.java
@@ -1,5 +1,8 @@
 package com.jmagero.Marven;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car implements Vehicle {
 	public void drive() {
 		System.out.print("I am driving a car");
