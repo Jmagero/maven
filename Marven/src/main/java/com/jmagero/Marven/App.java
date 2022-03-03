5ef78df5c74ed4082fd633d684d8184a615f51a8
Jocyline Magero <jmagero54@gmail.com>
2022-03-03 09:23:46 -0600

    Autowired Annotation

--- Marven/src/main/java/com/jmagero/Marven/App.java
+++ Marven/src/main/java/com/jmagero/Marven/App.java
@@ -12,7 +12,9 @@ public class App
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-    	Vehicle obj = (Vehicle)context.getBean("bike");
-    	obj.drive();
+    	Car obj = (Car)context.getBean("car");
+		obj.drive();
+//    	Tyre t = (Tyre)context.getBean("tyre");
+//    	System.out.print(t);
     }
 }
--- Marven/src/main/java/com/jmagero/Marven/Car.java
+++ Marven/src/main/java/com/jmagero/Marven/Car.java
@@ -1,10 +1,14 @@
 package com.jmagero.Marven;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements Vehicle {
+	@Autowired
+	private Tyre tyre;
 	public void drive() {
-		System.out.print("I am driving a car");
+		System.out.print("I am driving " + "its working");
 	}
+	
 }
